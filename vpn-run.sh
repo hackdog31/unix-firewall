37a3bf652e196e7ab87d9ec76c1fafc621a0e832
hackdog31 <maxanthony.pablea@gmail.com>
2020-11-30 18:45:29 +0800

    executable files bash scripts

---
+++ vpn-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo ufw disable
+sudo ufw reset
+sudo ufw default deny outgoing
+
+sudo ufw allow out on <interface>
+sudo ufw allow out 53/udp comment "dns trffic" #dns traffic
+sudo ufw allow out on <interface> to any port 80 comment "http traffic" #http traffic
+sudo ufw allow out on <interface> to any port 443 comment "https traffic" #https traffic
+sudo ufw allow out to <vpn-ip> port <vpn-port> proto udp
+
+sudo ufw enable
+sudo ufw reload
+sudo systemctl restart network-manager
+sudo openvpn --auth-nocache --mssfix 1450 --config /home/hakdog/Documents/vpn-openvpn/internet-vpn.ovpn
