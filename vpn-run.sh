72e0fdc9ac883972b3e006374e7be75387d445b3
maxanthony pablea <maxanthonypablea>
2021-01-12 11:02:29 +0800

    ufw rule killswitch updated

--- vpn-run.sh
+++ vpn-run.sh
@@ -5,7 +5,7 @@ sudo ufw reset
 sudo ufw default deny outgoing
 
 sudo ufw allow out on <interface>
-sudo ufw allow out 53/udp comment "dns trffic" #dns traffic
+sudo ufw allow in on <vpn-interface> from any to any port 53 proto tcp comment 'dns traffic' #dns traffic
 sudo ufw allow out on <interface> to any port 80 comment "http traffic" #http traffic
 sudo ufw allow out on <interface> to any port 443 comment "https traffic" #https traffic
 sudo ufw allow out to <vpn-ip> port <vpn-port> proto udp
